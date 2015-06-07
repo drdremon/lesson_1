87cfd9bf6227c873c01ec9a35730bec5db7f1589
drdremon <86_motor@mail.ru>
2015-06-07 12:33:19 +0300

    AutoLoad

---
+++ www/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+function __autoload($class){
+    if (file_exists(__DIR__.'/controller/'.$class.'.php')){
+        require __DIR__.'/controller/'.$class.'.php';
+    } elseif (file_exists(__DIR__.'/model/'.$class.'.php')){
+        require __DIR__.'/model/'.$class.'.php';
+    }
+}
